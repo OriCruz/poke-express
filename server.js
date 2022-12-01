520fb862e0c41d983074e1d2b7226ff5ed81a3e5
OriCruz <oc080901@gmail.com>
2022-12-01 11:05:54 -0500

    My server is set up and running

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+//define root route
+app.get('/', (req,res)=>{
+    res.send('Welcome to the Pokemon App!');
+});
 
 //app listening for port
 app.listen(port, ()=>{
